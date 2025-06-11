26a586c376c6efd2d04349415eee98e3d4e2b5c7
o000.ruslan@yandex.ru <o000.ruslan@yandex.ru>
2025-06-11 19:07:42 +0300

    Правки после checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,8 +5,13 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     List<Film> getAllFilms();
+
     Film getFilmById(int id);
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,8 +5,13 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     List<User> getAllUsers();
+
     User getUserById(int id);
+
 }
